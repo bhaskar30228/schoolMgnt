af94be3e01c0b434a1753f72dba912656646230c
Bhaskar Pujari <bhaskarpujari2006@gmail.com>
2025-05-23 19:50:23 +0530

    added to gitignore

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '9mq2vhvm',
-  database: 'school'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = pool.promise(); 
